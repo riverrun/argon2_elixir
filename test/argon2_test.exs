5c92e4013d22cc741459fdd48a3106a8ff4a6ccc
David Whitlock <alovedalongthe@gmail.com>
2016-10-22 19:47:07 +0800

    Updated tests

--- test/argon2_test.exs
+++ test/argon2_test.exs
@@ -1,39 +1,33 @@
 defmodule Argon2Test do
   use ExUnit.Case
 
-  def permute([]), do: [[]]
-  def permute(list) do
-    for x <- list, y <- permute(list -- [x]), do: [x|y]
-  end
-
-  def hash_check_password(password, wrong1, wrong2, wrong3) do
-    for argon2_type <- 0..2 do
-      hash = Argon2.hash_pwd_salt(password, [argon2_type: argon2_type])
-      assert Argon2.verify_hash(hash, password, [argon2_type: argon2_type]) == true
-      assert Argon2.verify_hash(hash, wrong1, [argon2_type: argon2_type]) == false
-      assert Argon2.verify_hash(hash, wrong2, [argon2_type: argon2_type]) == false
-      assert Argon2.verify_hash(hash, wrong3, [argon2_type: argon2_type]) == false
-    end
-  end
+  import Argon2TestHelper
 
   test "hashing and checking passwords" do
-    hash_check_password("password", "passwor", "passwords", "pasword")
-    hash_check_password("hard2guess", "ha rd2guess", "had2guess", "hardtoguess")
+    hash_check("password")
+    hash_check("hard2guess")
   end
 
   test "hashing and checking passwords with characters from the extended ascii set" do
-    hash_check_password("aáåäeéêëoôö", "aáåäeéêëoö", "aáåeéêëoôö", "aáå äeéêëoôö")
-    hash_check_password("aáåä eéêëoôö", "aáåä eéê ëoö", "a áåeé êëoôö", "aáå äeéêëoôö")
+    hash_check("aáåäeéêëoôö")
+    hash_check("aáåä eéêëoôö")
   end
 
   test "hashing and checking passwords with non-ascii characters" do
-    hash_check_password("Сколько лет, сколько зим", "Сколько лет,сколько зим",
-                        "Сколько лет сколько зим", "Сколько лет, сколько")
-    hash_check_password("สวัสดีครับ", "สวัดีครับ", "สวัสสดีครับ", "วัสดีครับ")
+    hash_check("Сколько лет, сколько зим")
+    hash_check("สวัสดีครับ")
   end
 
   test "hashing and checking passwords with mixed characters" do
-    hash_check_password("Я❤três☕ où☔", "Я❤tres☕ où☔", "Я❤três☕où☔", "Я❤três où☔")
+    hash_check("Я❤três☕ où☔")
+  end
+
+  test "known test vectors" do
+    for {arg, result} <- known_vectors() do
+      assert Argon2.hash_password("password", "somesalt",
+                                  t_cost: 2, m_cost: 16, parallelism: 4,
+                                  hashlen: 24, encoded: arg) == result
+    end
   end
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,33 @@
 ExUnit.start()
+
+defmodule Argon2TestHelper do
+  use ExUnit.Case
+
+  def hash_check(password) do
+    wrong_list = wrong_passwords(password)
+    for argon2_type <- 0..2 do
+      hash = Argon2.hash_pwd_salt(password, [argon2_type: argon2_type])
+      assert Argon2.verify_hash(hash, password, [argon2_type: argon2_type])
+      for wrong <- wrong_list do
+        refute Argon2.verify_hash(hash, wrong, [argon2_type: argon2_type])
+      end
+    end
+  end
+
+  def known_vectors do
+    [{0, "45d7ac72e76f242b20b77b9bf9bf9d5915894e669a24e6c6"},
+     {1, "$argon2i$v=19$m=65536,t=2,p=4$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG"}]
+  end
+
+  defp wrong_passwords(password) do
+    (for num <- 2..6, do: String.duplicate(password, num)) ++
+    (for num <- 0..9, do: password <> Integer.to_string(num)) ++
+    slices(password) ++ [String.reverse(password), ""]
+  end
+
+  defp slices(password) do
+    ranges = [{1, -1}, {0, -2}, {2, -1}, {0, -3}, {2, -2}, {1, -3},
+              {3, -2}, {2, -3}]
+    for {first, last} <- ranges, do: String.slice(password, first..last)
+  end
+end
