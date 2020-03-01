75f9c8449552d24274560526526b465714f46b31
David Whitlock <alovedalongthe@gmail.com>
2020-03-01 11:06:39 +0700

    Using comeonin v5.3, which changes add_hash so that it does not set the password to nil

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v2.3.0
+
+* Changes
+  * using Comeonin v5.3, which changes `add_hash` so that it does NOT set the password to nil
+
 ## v2.2.0
 
 * Enhancements
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Argon2.Mixfile do
   use Mix.Project
 
-  @version "2.2.1"
+  @version "2.3.0"
 
   @description """
   Argon2 password hashing algorithm for Elixir
@@ -32,8 +32,8 @@ defmodule Argon2.Mixfile do
 
   defp deps do
     [
-      {:comeonin, "~> 5.2"},
-      {:elixir_make, "~> 0.5", runtime: false},
+      {:comeonin, "~> 5.3"},
+      {:elixir_make, "~> 0.6", runtime: false},
       {:ex_doc, "~> 0.20", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false}
     ]
