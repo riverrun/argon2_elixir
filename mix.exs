b98edf9fc871902fe49abf1d07dd3b5487510de8
Kian-Meng, Ang <kianmeng@cpan.org>
2021-12-21 14:08:53 +0800

    Misc doc changes
    Besides other documentation changes, this commit includes all relevant
    documents to the generated HTML doc for HexDocs.pm and leverages on
    latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule Argon2.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/riverrun/argon2_elixir"
+
   @version "2.4.0"
 
   @description """
@@ -18,8 +20,8 @@ defmodule Argon2.Mixfile do
       make_clean: ["clean"],
       description: @description,
       package: package(),
-      source_url: "https://github.com/riverrun/argon2_elixir",
       deps: deps(),
+      docs: docs(),
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
@@ -36,17 +38,44 @@ defmodule Argon2.Mixfile do
     [
       {:comeonin, "~> 5.3"},
       {:elixir_make, "~> 0.6", runtime: false},
-      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
-      files: ["lib", "c_src", "argon2/include", "argon2/src", "mix.exs", "Makefile*", "README.md"],
+      files: [
+        "lib",
+        "c_src",
+        "argon2/include",
+        "argon2/src",
+        "mix.exs",
+        "Makefile*",
+        "README.md",
+        "LICENSE.md",
+        "CHANGELOG.md"
+      ],
       maintainers: ["David Whitlock"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/riverrun/argon2_elixir"}
+      links: %{
+        "Changelog" => "https://hexdocs.pm/argon2_elixir/changelog.html",
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [],
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
